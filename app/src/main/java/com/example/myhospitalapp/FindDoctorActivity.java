16c853d1beda3fcecbd024721952416aea05f7b1
ian otieno <ianotieno23@gmail.com>
2025-07-16 13:15:06 +0300

    update call dr

--- app/src/main/java/com/example/myhospitalapp/FindDoctorActivity.java
+++ app/src/main/java/com/example/myhospitalapp/FindDoctorActivity.java
@@ -1,9 +1,13 @@
 package com.example.myhospitalapp;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,5 +20,14 @@ public class FindDoctorActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_find_doctor);
 
+        CardView exit = findViewById(R.id.cardFDBack);
+
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FindDoctorActivity.this,HomeActivity.class));
+            }
+        });
     }
 }
